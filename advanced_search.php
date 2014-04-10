dd42307496d667076f6fc0e167dbf16d464cd29c
Steve Ludovicy <steve@gms.lu>
2014-04-10 09:56:07 +0200

    Bump version

--- advanced_search.php
+++ advanced_search.php
@@ -2,7 +2,7 @@
 /**
  * Processing an advanced search over an E-Mail Account
  *
- * @version 2.1.5
+ * @version 2.1.6
  * @licence GNU GPLv3+
  * @author  Wilwert Claude
  * @author  Ludovicy Steve
